8358870abcb8148ff642610c761066836f8a5b3f
Nothing <22212484+OswinWu@users.noreply.github.com>
2022-05-17 20:22:29 +0800

    [feature] add ants for handler job submit

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/pzqf/zEngine
 
 go 1.17
 
-require github.com/pzqf/zUtil v0.0.0-20220510065844-6cd0a0c36238 // indirect
+require github.com/pzqf/zUtil v0.0.0-20220513071314-ccdedab5cbbe
+
+require github.com/panjf2000/ants v1.3.0 // indirect
--- zNet/dispatcher.go
+++ zNet/dispatcher.go
@@ -1,10 +1,16 @@
 package zNet
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/panjf2000/ants"
+)
 
 type HandlerFun func(session *Session, packet *NetPacket)
 
 var mapHandler = make(map[int32]HandlerFun)
+var defaultPoolSize = 10000
+var workerPool *ants.Pool
 
 func RegisterHandler(protoId int32, fun HandlerFun) error {
 	if _, ok := mapHandler[protoId]; ok {
@@ -19,17 +25,23 @@ func Dispatcher(session *Session, netPacket *NetPacket) error {
 	if netPacket == nil {
 		return errors.New("packet is nil")
 	}
+
 	fun, ok := mapHandler[netPacket.ProtoId]
 	if !ok {
 		return errors.New("no handlerFun")
 	}
-
-	fun(session, netPacket)
-
+	workerPool.Submit(func() {
+		fun(session, netPacket)
+	})
 	return nil
 }
 
 func init() {
+	p, err := ants.NewPool(defaultPoolSize)
+	if err != nil {
+		panic(err)
+	}
+	workerPool = p
 	_ = RegisterHandler(0, heartbeat)
 }
 
